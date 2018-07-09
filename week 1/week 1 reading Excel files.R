a6224eac8ee5107e798295c07fa37d254f6e0f53
jhd6242 <justin.decross@gmail.com>
2018-07-08 21:14:02 -0400

    notes continued

--- week 1/week 1 reading Excel files.R
+++ week 1/week 1 reading Excel files.R
@@ -1,1 +1,11 @@
 # week 1 reading Excel files
+if (!file.exists("C:/Users/wheel/OneDrive/Documents/Justin/Justin/Documents/COURSERA/gettingandcleaning/project/week 1")) {
+  dir.create("C:/Users/wheel/OneDrive/Documents/Justin/Justin/Documents/COURSERA/gettingandcleaning/project/week 1")
+}
+file_URL <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xml?accessType=DOWNLOAD"
+download.file(file_URL,destfile = "C:/Users/wheel/OneDrive/Documents/Justin/Justin/Documents/COURSERA/gettingandcleaning/project/week 1/cameras.xlsx", method = "curl")
+date_Downloaded <- date()
+date_Downloaded
+# read.xlsx(), read.xlsx2() {xlsx package}
+library(xlsx)
+camera_Data <-
