e6c2b54592e59af119c9f62a979ec8cbb270d111
Justin DeCross <jdecross@Excellus.com>
2018-07-09 11:49:05 -0400

    updated notes and starting a markdown document

--- week 1/week 1 reading Excel files.R
+++ week 1/week 1 reading Excel files.R
@@ -1,11 +1,13 @@
 # week 1 reading Excel files
-if (!file.exists("C:/Users/wheel/OneDrive/Documents/Justin/Justin/Documents/COURSERA/gettingandcleaning/project/week 1")) {
-  dir.create("C:/Users/wheel/OneDrive/Documents/Justin/Justin/Documents/COURSERA/gettingandcleaning/project/week 1")
+if (!file.exists("C:/Users/jdecross/Documents/COURSERA/project_1/week 1")) {
+  dir.create("C:/Users/jdecross/Documents/COURSERA/project_1/week 1")
 }
 file_URL <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xml?accessType=DOWNLOAD"
-download.file(file_URL,destfile = "C:/Users/wheel/OneDrive/Documents/Justin/Justin/Documents/COURSERA/gettingandcleaning/project/week 1/cameras.xlsx", method = "curl")
+download.file(file_URL, destfile = "C:/Users/jdecross/Documents/COURSERA/project_1/week 1/cameras.xml")
 date_Downloaded <- date()
 date_Downloaded
 # read.xlsx(), read.xlsx2() {xlsx package}
-library(xlsx)
-# camera_Data <-
+library(readxl)
+require(XML)
+camera_Data <- xmlParse("C:/Users/jdecross/Documents/COURSERA/project_1/week 1/cameras.xml")
+head(camera_Data)
