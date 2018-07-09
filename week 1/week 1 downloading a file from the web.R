46a33d84cee84baaa381e41e55f907d568547fc8
jhd6242 <justin.decross@gmail.com>
2018-07-08 20:21:45 -0400

    editing paths

--- week 1/week 1 downloading a file from the web.R
+++ week 1/week 1 downloading a file from the web.R
@@ -1,8 +1,8 @@
 #downloading files
 #Downloading a file from the web
 file_URL <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
-download.file( file_URL, destfile = "C:/Users/wheel/OneDrive/Documents/Justin/Justin/Documents/COURSERA/gettingandcleaning/week 1/cameras.csv", method = "curl")
-list.files("C:/Users/wheel/OneDrive/Documents/Justin/Justin/Documents/COURSERA/gettingandcleaning/week 1")
+download.file( file_URL, destfile = "C:/Users/wheel/OneDrive/Documents/Justin/Justin/Documents/COURSERA/gettingandcleaning/project/week 1/cameras.csv", method = "curl")
+list.files("C:/Users/wheel/OneDrive/Documents/Justin/Justin/Documents/COURSERA/gettingandcleaning/project/week 1")
 
 date_Downloaded <- date()
 date_Downloaded
